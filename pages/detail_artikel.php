906b7f1a98887e28c4c78e3143251ebb63947c2d
dwiprst13 <dwiprst13@gmail.com>
2023-12-21 14:19:25 +0700

    sa

--- pages/detail_artikel.php
+++ pages/detail_artikel.php
@@ -16,12 +16,12 @@ if ($result && mysqli_num_rows($result) > 0) {
             <div class="w-[100%] flex mx-auto grid grid-cols-12 gap-8 pt-10 pb-8 py-3">
                 <div class="visi p-5 lg:col-span-9 bg-white border border-gray-900 rounded-lg ">
                     <h1 class="text-3xl text-center py-3"><b><?= $row_artikel['judul'] ?></b></h1>
-                    <img src="<?= $baru_link ?>" alt="Gambar Artikel" class="">
+                    <img src="<?= $baru_link ?>" alt="Gambar Artikel" class="w-full">
                     <p class="py-3"><?= $row_artikel['content'] ?></p>
                 </div>
                 <div class="visi p-5 lg:col-span-3 bg-white border border-gray-900 rounded-lg h-auto self-start">
                     <?php
-                    $queryArtikel = mysqli_query($conn, "SELECT * FROM artikel WHERE status = 'publish'");
+                    $queryArtikel = mysqli_query($conn, "SELECT * FROM artikel WHERE status = 'publish' AND id_artikel != '$id_artikel' ORDER BY id_artikel DESC LIMIT 5");
                     while ($row_artikel = mysqli_fetch_assoc($queryArtikel)) {
                     ?>
                         <a class="line-clamp-2 p-1 hover:text-blue-500" href="?page=detail_artikel&id_artikel=<?= $row_artikel['id_artikel'] ?>">
